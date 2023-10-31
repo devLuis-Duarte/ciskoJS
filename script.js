5e179a68d22673c0ce404e07b1daa66a244a7407
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2023-10-31 14:27:46 -0300

    variaveis do tipo const

--- script.js
+++ script.js
@@ -6,5 +6,38 @@
 let height;
 
 console.log(height)
+
+//inicializacao de variaveis, é atribuido um valor a ela(s)
+
+let h = 180;
+let anotherheight = h;
+let weight;
+
+console.log(h)
+console.log(anotherheight)
+weight = 70
 console.log(weight)
 
+//Alterando valor de variaveis
+let steps = 100;
+console.log(steps)
+steps = 120;
+console.log(steps)
+steps = steps+200;
+console.log(steps)
+
+let greeting = "Hello World";
+let counter = 100;
+
+console.log(greeting)
+greeting = 1;
+console.log(greeting)
+
+greeting = "Hello World"
+greeting = greeting + counter;
+console.log(greeting) 
+
+//tipos de variaveis com valores constantes, ou seja, tipo const
+
+const g = "Hello";
+g = "Hi";
