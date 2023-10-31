455663f344152a0c95da589efac4659a4b305b4c
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2023-10-31 12:07:35 -0300

    variaveis no javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+//variaveis
+//var ou let sao variaveis e podem ter seus valores alterados, var pode ter mais de uma variavel com mesmo nome
+// enquanto let nao possui esse problema, por isso let acaba sendo mais recomendado para ser usado em vez de var
+//variaveis do tipo const possuem valores constantes, por isso deixam de ser variaveis
+
+let height;
+
+console.log(height)
+console.log(weight)
+
